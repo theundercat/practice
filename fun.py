9550b834a61f7e39321c86fc2aeb3c6ad144b195
theundercat <maggo8@protonmail.com>
2024-11-04 02:38:50 +0100

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,12 +1,20 @@
 import streamlit as st
 
-offer = st.text_input("Price Offer:")
+offer = st.text_input("Price Offer: ")
 tcost = st.text_input("Total cost:")
 
 if offer and tcost:
     try:
         offer = int(offer)
         tcost = int(tcost)
-        st.write(f"O.P: {(offer - tcost)/offer*100:.2f}%")
+        
+        # Calculate the operating profit percentage
+        op_percentage = (offer - tcost) / offer * 100
+        st.write(f"O.P: {op_percentage:.2f}%")
+        
+        # Check if O.P is over 10%
+        if op_percentage > 10:
+            st.write("That's pretty good!")
+            
     except ValueError:
-        st.write("Numeric values only bro.")
+        st.write("Numeric values only, bro.")
